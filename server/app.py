d441449c7cb78b95fea1245c86f1f82222b79dbb
Emmanuel <emwanzia532@gmail.com>
2023-09-30 03:11:23 +0300

    Completed assignmnet

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, request, session
+from flask import Flask, make_response, jsonify, request, session, Response  # Import Response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -18,13 +18,43 @@ db.init_app(app)
 
 api = Api(app)
 
+class UserResource(Resource):
+
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            session['user_id'] = user.id
+            return jsonify(user.to_dict()), 200
+        else:
+            return jsonify({'message': 'User not found'}), 404
+
+class LogoutResource(Resource):
+
+    def delete(self):
+        if 'user_id' in session:
+            session.pop('user_id', None)
+            return {}, 204
+        else:
+            return jsonify({'message': 'Not logged in'}), 401
+
+class CheckSessionResource(Resource):
+
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return jsonify(user.to_dict()), 200
+        return jsonify({'message': 'Unauthorized'}), 401
+
 class ClearSession(Resource):
 
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
-
+        session.clear()
         return {}, 204
 
 class IndexArticle(Resource):
@@ -40,18 +70,22 @@ class ShowArticle(Resource):
         session['page_views'] += 1
 
         if session['page_views'] <= 3:
-
             article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
+            if article:
+                # Create a Response object and jsonify it
+                article_json = jsonify(article.to_dict())
+                return make_response(article_json, 200)
+            else:
+                return jsonify({'message': 'Article not found'}), 404
 
-            return make_response(article_json, 200)
-
-        return {'message': 'Maximum pageview limit reached'}, 401
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(UserResource, '/login')
+api.add_resource(LogoutResource, '/logout')
+api.add_resource(CheckSessionResource, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
